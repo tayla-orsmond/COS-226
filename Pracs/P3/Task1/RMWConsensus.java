0046dbc545eff514959ce41e2edfd70b577086ae
taylawantsasnack <tayla.orsmond@gmail.com>
2022-09-06 09:37:29 +0200

    Output which thread wins

--- Pracs/P3/Task1/RMWConsensus.java
+++ Pracs/P3/Task1/RMWConsensus.java
@@ -11,6 +11,7 @@ public class RMWConsensus<T> extends ConsensusProtocol<T>{
         System.out.println("]");
         if (proposed[Integer.parseInt(Thread.currentThread().getName())] != proposed[1 - Integer.parseInt(Thread.currentThread().getName())]){//Am I first?
 		    proposed[1 - Integer.parseInt(Thread.currentThread().getName())] = proposed[Integer.parseInt(Thread.currentThread().getName())]; //Yes, return my input
+            System.out.println("\t\t\tThread " + Thread.currentThread().getName() + " is first!");
         }
             //else I lost (am second), return the other thread's input (which is now mine too)
         System.out.println("Thread " + Thread.currentThread().getName() + " decided " + proposed[0]);
